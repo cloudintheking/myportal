5a7f747971d5848a29b6d40c152ecca69ada2bea
Cloudintheking <2424897346@qq.com>
2018-08-24 17:25:01 +0800

    1

---
+++ src/app/font-end/rich-text/rich-text.component.ts
@@ -0,0 +1,37 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+import {BackApiService} from '../../service/back-api.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-rich-text',
+  templateUrl: './rich-text.component.html',
+  styleUrls: ['./rich-text.component.css']
+})
+export class RichTextComponent implements OnInit {
+  contents: string; // 富文本内容
+  // 监听键盘按下事件
+  @HostListener('document:keydown', ['$event'])
+  onkeydown(event) {
+    const keyCode = event.keyCode;
+    console.log(keyCode);
+  }
+
+  constructor(private richApi: BackApiService, private  routerInfo: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.routerInfo.params.subscribe(data => {
+      this.richApi.getHeaderImgsAnon().subscribe(
+        success => {
+          if (data.id === '1') {// 公司介绍
+            this.contents = success.prop.introduction;
+          } else { // 联系我们
+            this.contents = success.prop.contractUs;
+          }
+        });
+
+      console.log(data);
+    });
+  }
+
+}
